17b1fdf8d93a8790c9c83e82d4e80898573c76e6
Miguel Tibincoski <tibincoskimiguel@gmail.com>
2025-06-21 10:27:56 -0300

    feat: setup shadcn-ui and font-family

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter, Outlet, Route, Routes } from "react-router";
+import "./global.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HashRouter>
+      <Routes>
+        <Route index element={<h1>Home Page</h1>} />
+        <Route path="accounts" element={<h1>Accounts Page</h1>} />
+
+        <Route
+          path="dashboard"
+          element={
+            <>
+              <h1>Dashboard Page</h1>
+              <br />
+              <br />
+              <Outlet />
+            </>
+          }
+        >
+          <Route index element={<h2>Home Page</h2>} />
+          <Route path="pix" element={<h2>Pix Page</h2>} />
+
+          <Route
+            path="payments"
+            element={
+              <>
+                <h2>Payments Page</h2>
+                <br />
+                <Outlet />
+              </>
+            }
+          >
+            <Route index element={<h3>Home Page</h3>} />
+            <Route path="details" element={<h3>Payment Details Page</h3>} />
+          </Route>
+        </Route>
+      </Routes>
+    </HashRouter>
+  </StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   base: "/asaas-web-client",
   build: {
     outDir: "build",
